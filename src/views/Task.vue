a15857364de9124576486e35b67d90191b1dc299
Marcio Roberto <mroberto@gmail.com>
2020-04-19 00:32:38 -0300

    adding and editing tasks

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="task-view">
     <div class="flex flex-col flex-grow items-start justify-between px-4">
-      {{ task.name }}
+      <input type="text" 
+          class="p2 w-full mr-2 block text-xl font-bold"
+          :value="task.name"
+          @change="updateTaskProperty($event, 'name')"
+          @keyup.enter="updateTaskProperty($event, 'name')">
+      
+      <textarea placeholder="Enter task description"
+              class="relative bg-transparent px-2 mt-2 h-64 border-none leading-normal"
+              :value="task.description"
+              @change="updateTaskProperty($event, 'description')"></textarea>
+
+
     </div>
+    
   </div>
 </template>
 
@@ -15,6 +27,15 @@ export default {
         'getTask']),
     task() {
       return this.getTask(this.$route.params.id)
+    }
+  },
+  methods: {
+    updateTaskProperty(e, key) {
+      this.$store.commit('UPDATE_TASK', {
+        task: this.task,
+        key,
+        value: e.target.value
+      })
     }
   }
 }
